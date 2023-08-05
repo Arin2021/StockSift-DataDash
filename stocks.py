05ae4c7de11de5325b139b73cb262c1dd487614f
Arin2021 <110546030+Arin2021@users.noreply.github.com>
2023-08-05 11:49:22 +0530

    Add files via upload

---
+++ stocks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+stock_symbols = [
+    'BRITANNIA.NS', 'TATACONSUM.NS', 'TITAN.NS', 'NESTLEIND.NS', 'ITC.NS',
+    'HEROMOTOCO.NS', 'ULTRACEMCO.NS', 'APOLLOHOSP.NS', 'ADANIENT.NS', 'BAJAJFINSV.NS',
+    'BAJFINANCE.NS', 'TATASTEEL.NS', 'ONGC.NS', 'MARUTI.NS', 'HINDALCO.NS',
+    'KOTAKBANK.NS', 'NTPC.NS', 'LT.NS', 'TCS.NS', 'RELIANCE.NS',
+    'LTIM.NS', 'HDFCLIFE.NS', 'COALINDIA.NS', 'BHARTIARTL.NS', 'BAJAJ-AUTO.NS',
+    'INDUSINDBK.NS', 'WIPRO.NS', 'TECHM.NS', 'CIPLA.NS'
+    # Add more stock symbols as needed
+]
+
+
+# In[ ]:
+
+
+
+
